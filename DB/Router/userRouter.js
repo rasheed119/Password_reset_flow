a5435cd38162a857f91bf5960de8ce39338c95d0
rasheed119 <rasheed20052000@gmail.com>
2023-07-21 19:07:23 +0530

    change in node mailerport

--- DB/Router/userRouter.js
+++ DB/Router/userRouter.js
@@ -50,8 +50,8 @@ router.put("/forgot-password", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port : 587,
-      secure:false ,
+      port : 465,
+      secure : true,
       auth: {
         user: sender_mail,
         pass: passkey,
