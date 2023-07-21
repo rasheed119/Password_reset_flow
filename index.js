db70a4c5a89c8b17df43573a934e19cb1a04d85e
rasheed119 <rasheed20052000@gmail.com>
2023-07-21 16:34:23 +0530

    .

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/user", userRouter);
 
-mongoose
-  .connect(process.env.mongourl)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.log("Error Connecting to MongoDB", error.message));
+mongoose.connect(process.env.mongourl);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started in the port localhost:${process.env.PORT}`)
